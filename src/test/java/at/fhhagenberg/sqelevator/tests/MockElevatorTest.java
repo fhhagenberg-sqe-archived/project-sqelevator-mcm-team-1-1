6d33fd1538040e4424a4fda33c0ee3acac358d60
sourceclimber <11772869+sourceclimber@users.noreply.github.com>
2020-01-07 19:24:45 +0800

    add junit, surefire and jacoco maven plugins and add jupiter-engine dependency

---
+++ src/test/java/at/fhhagenberg/sqelevator/tests/MockElevatorTest.java
@@ -0,0 +1,13 @@
+package at.fhhagenberg.sqelevator.tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MockElevatorTest {
+
+    @Test
+    void testInitConditions() {
+        assertTrue(true);
+    }
+}
